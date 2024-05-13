a7afc96ecb7e93feea821b19a420f1f14c0baf9e
Jaswir <jaswirraghoe@gmail.com>
2024-05-13 22:51:35 +0200

    Get english transcription from English youtube video

--- get_youtube_audio.py
+++ get_youtube_audio.py
@@ -1,12 +1,29 @@
 from pytube import YouTube
+from openai import OpenAI
+from os import environ
+
+OpenAI_API_KEY = environ.get("OPENAI_API_KEY")
+client = OpenAI(OpenAI_API_KEY)
+
+
+def get_english_transcription_from_english_youtube(video):
+    try:
+        stream = video.streams.filter(only_audio=True).first()
+        stream.download(filename=f"input_video.mp3")
+        print("The video is downloaded in MP3")
+    except KeyError:
+        print(
+            "Unable to fetch video information. Please check the video URL or your network connection."
+        )
+
+    audio_file = open("input_video.mp3", "rb")
+    transcription = client.audio.transcriptions.create(
+        model="whisper-1", file=audio_file
+    )
+
+    return transcription.text
+
 
 url = "https://www.youtube.com/watch?v=lXbEixkHcgc"
 video = YouTube(url)
-
-try:
-   video = YouTube(url)
-   stream = video.streams.filter(only_audio=True).first()
-   stream.download(filename=f"{video.title}.mp3")
-   print("The video is downloaded in MP3")
-except KeyError:
-   print("Unable to fetch video information. Please check the video URL or your network connection.")
+transcription = get_english_transcription_from_english_youtube(video)
