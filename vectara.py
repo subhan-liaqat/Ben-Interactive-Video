6dc1c14b1244262fecd9f06bb208308bec80d93c
Jaswir <jaswirraghoe@gmail.com>
2024-05-14 22:35:37 +0200

    Made function to interact with video transcript

--- vectara.py
+++ vectara.py
@@ -1,22 +1,8 @@
 import requests
 import json
 
-
-# Step 1 Create Corpus
-# url = "https://api.vectara.io/v1/create-corpus"
-
-# payload = json.dumps(
-#     {"corpus": {"name": "Interactive Video", "description": "Interactive Video"}}
-# )
-# headers = {
-#     "Content-Type": "application/json",
-#     "Accept": "application/json",
-#     "x-api-key": "zut_IccVS9aWrgH6-s9K--BKSt9pVfYKgClXR8j3cg",
-# }
-
-# response = requests.request("POST", url, headers=headers, data=payload)
-
-# print(response.text)
+customerID = "566695243"
+api_key = "zut_IccVS9aWrgH6-s9K--BKSt9pVfYKgClXR8j3cg"
 
 
 # Step 1 Reset Corpus
@@ -54,5 +40,52 @@ def AddVideoTranscription():
     print(response.text)
 
 
+# Step 3 Ask Question
+def askQuestion(prompt):
+    url = "https://api.vectara.io/v1/query"
+
+    payload = json.dumps(
+        {
+            "query": [
+                {
+                    "query": "Why are computers bad at math?",
+                    "start": 0,
+                    "numResults": 3,
+                    "contextConfig": {
+                        "sentences_before": 3,
+                        "sentences_after": 3,
+                        "start_tag": "<b>",
+                        "end_tag": "</b>",
+                    },
+                    "corpusKey": [{"corpus_id": 15}],
+                    "summary": [{"max_summarized_results": 10, "response_lang": "en"}],
+                }
+            ]
+        }
+    )
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "customer-id": "566695243",
+        "x-api-key": "zut_IccVS9aWrgH6-s9K--BKSt9pVfYKgClXR8j3cg",
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    response_data = response.json()
+    result = response_data["responseSet"][0]["summary"][0]
+    RawAnswer = result["text"]
+
+    return RawAnswer
+
+
+
 ResetCorpus()
 AddVideoTranscription()
+
+question = "Why are computers bad at math?"
+answer = askQuestion(question)
+
+
+
+print(answer)
