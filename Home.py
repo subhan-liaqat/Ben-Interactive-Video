a2acd11a9ebb1cb35c1b655876975d3993128e20
Jaswir <jaswirraghoe@gmail.com>
2024-05-16 17:42:50 +0200

    Improved Home page

--- Home.py
+++ Home.py
@@ -6,16 +6,19 @@ st.title("Welcome To Ben Interactive Video 📺")
 
 st.divider()
 
-st.write("Introducing Ben Interactive Video: Video Interaction for Benin and Beyond We are proud to present Ben Interactive Video. Designed specifically for the Benin Multimodal AI Hackathon. Our innovative solution empowers users to interact with videos in their preferred language, saving time and breaking language barriers. also Creating new Media With Subtitles and Dubbing 🎥")
+st.write("""Introducing Ben Interactive Video: Video Interaction for Benin and Beyond. Interact with videos in your preferred language, Ask Questions about the Video, Subtitles and Dubbing 🎥.
+""")
 
 st.subheader("Features")
 
 st.markdown(
     """
-    📁 **Upload a video from YouTube:** Easily upload video from a Youtube URL\n
-    💭 **Translate the video to Yoruba or French:** Create the full transcript translated into the selected language\n
-    🎥 **Choose to have the video dubbed or subtitled or both:**  choose whether the video uploaded is subbed or dubbed into your selected langauge or both\n 
-    📺 **Watch the translated video:** A New Video for the user to watch within the app\n
+    📁 **Upload a video from YouTube URL** \n
+    ❔ **Interact / Ask Questions about the Video in your Preferred Language**\n
+    💭 **Translate to Yoruba or French** \n
+    🎥 **Choose to have the video Dubbed or Subtitled or both** \n
+    ⌚ **Saves time so you don’t have to watch the entire video** \n 
+    
     """
 )
 
