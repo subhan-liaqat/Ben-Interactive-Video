b521ac951029850783d50c4256ff598916234ed9
Rowan Wood <mrdiamonddirt@gmail.com>
2024-05-16 16:12:58 +0100

    feat: Update landing page content and instructions

--- Home.py
+++ Home.py
@@ -2,12 +2,32 @@ import streamlit as st
 
 from streamlit_extras.stylable_container import stylable_container
 
-st.title("Welcome To Beninteractive Video 📺")
+st.title("Welcome To Ben Interactive Video 📺")
 
-st.write("This is a video translation app that allows you to translate a video from English to Yoruba or French. You can also choose to have the video dubbed or subtitled. 🎥")
+st.divider()
+
+st.write("Introducing Ben Interactive Video: Video Interaction for Benin and Beyond We are proud to present Ben Interactive Video. Designed specifically for the Benin Multimodal AI Hackathon. Our innovative solution empowers users to interact with videos in their preferred language, saving time and breaking language barriers. also Creating new Media With Subtitles and Dubbing 🎥")
 
 st.subheader("Features")
 
-st.write("Subtitle Video: Easily automatically create transcripts and subtitles for your videos.")
+st.markdown(
+    """
+    📁 **Upload a video from YouTube:** Easily upload video from a Youtube URL\n
+    💭 **Translate the video to Yoruba or French:** Create the full transcript translated into the selected language\n
+    🎥 **Choose to have the video dubbed or subtitled or both:**  choose whether the video uploaded is subbed or dubbed into your selected langauge or both\n 
+    📺 **Watch the translated video:** A New Video for the user to watch within the app\n
+    """
+)
 
-st.write("Sub/Dub Video: Translate your video to another language and choose to have it subtitled or dubbed.")
+st.subheader("How to Use")
+
+st.markdown(
+    """
+    ✔ Select the tool you would like to use from the sidebar\n
+    🌐 Enter the URL of the video you would like to interact with\n
+    🌍 Select the language you would like to translate the video to\n
+    🎥 Click the 'Upload Video' button\n
+    🤖 Wait for the video to be processed\n
+    📈 Profit
+    """
+)
