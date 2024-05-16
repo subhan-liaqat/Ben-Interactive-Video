126bff0235956d0ede6d653e2eca526a6729e888
Jaswir <jaswirraghoe@gmail.com>
2024-05-16 07:40:05 +0200

    Commit subs

--- get_youtube_audio.py
+++ get_youtube_audio.py
@@ -2,18 +2,21 @@ from pytube import YouTube
 from openai import OpenAI
 from os import environ
 import vectara
-import google_translate
 import streamlit as st
 from pydub import AudioSegment
+import google_translate
+import subvideo
 
 OpenAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 maxDuration = st.secrets["LIMIT_DURATION"]
 client = OpenAI(api_key=OpenAI_API_KEY)
 
+
 def get_duration_pydub(file_path):
-   audio_file = AudioSegment.from_file(file_path)
-   duration = audio_file.duration_seconds
-   return duration
+    audio_file = AudioSegment.from_file(file_path)
+    duration = audio_file.duration_seconds
+    return duration
+
 
 def prepForVectara():
     vectara.ResetCorpus()
@@ -31,37 +34,29 @@ def get_english_transcription_from_english_youtube(url, language):
             "Unable to fetch video information. Please check the video URL or your network connection."
         )
 
-    audio_file_path = "input_video.mp3"
-    audio_file = open(audio_file_path, "rb")
-    duration = get_duration_pydub(audio_file_path)
+    audio_file = open("input_video.mp3", "rb")
+    transcription = client.audio.transcriptions.create(
+        model="whisper-1", file=audio_file
+    )
 
-    print("Duration: ", duration)
-    if duration > maxDuration: 
-        print("The video is too long. Please upload a video that is less than 5 minutes.")
+    english_transcription = transcription.text
+    yoruba_transcription = google_translate.translate_english_to_yoruba(
+        english_transcription
+    )
 
+    # Write transcription to file
+    with open("video_transcription.txt", "w", encoding="utf-8") as file:
+        file.write(yoruba_transcription)
 
-    # transcription = client.audio.transcriptions.create(
-    #     model="whisper-1", file=audio_file
-    # )
+    print("Text has been written to video_transcription.txt")
 
-    # english_transcription = transcription.text
-    # yoruba_transcription = google_translate.translate_english_to_yoruba(
-    #     english_transcription
-    # )
+    prepForVectara()
 
-    # # Write transcription to file
-    # with open("video_transcription.txt", "w", encoding="utf-8") as file:
-    #     file.write(yoruba_transcription)
+    if language == "English":
+        return english_transcription
 
-    # print("Text has been written to video_transcription.txt")
-
-    # prepForVectara()
-
-    # if language == "English":
-    #     return english_transcription
-
-    # elif language == "Yoruba":
-    #     return yoruba_transcription
+    elif language == "Yoruba":
+        return yoruba_transcription
 
 
 def askQuestionAboutVideo(prompt, language):
---
+++ subvideo.py
@@ -0,0 +1,92 @@
+from pytube import YouTube
+from openai import OpenAI
+from os import environ
+import streamlit as st
+from pydub import AudioSegment
+import pysrt
+import ffmpeg
+import os
+import shutil
+
+import google_translate
+from google_translate import translate_english_to
+
+OpenAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+maxDuration = st.secrets["LIMIT_DURATION"]
+client = OpenAI(api_key=OpenAI_API_KEY)
+
+input_video = "input_video.mp4"
+
+# 0 Clear Temp Folder
+def clear_temp_folder():
+    if os.path.exists('temp'):
+        shutil.rmtree('temp')
+    os.makedirs('temp')
+
+# 1 Extract Audio from Youtube Video
+def extract_audio_from_youtube(url):
+    video = YouTube(url)
+
+    #write the video to a file
+    video.streams.get_highest_resolution().download(output_path="./temp", filename=input_video)
+
+    #extract audio from the video
+    audio_path = "audio_extract.mp3"
+    try:
+        stream = video.streams.filter(only_audio=True).first()
+        stream.download(filename=f"./temp/" + audio_path)
+        print("The video is downloaded in MP3")
+    except KeyError:
+        print(
+            "Unable to fetch video information. Please check the video URL or your network connection."
+        )
+
+# 2 Get SRT
+def Create_SRT():
+    audio_file_path = "./temp/audio_extract.mp3"
+    with open(audio_file_path, "rb") as audio_file:
+        transcription = client.audio.transcriptions.create(
+            model="whisper-1", file=audio_file, response_format="srt"
+        )
+    with open("temp/transcript.srt", "w") as transcript_file:
+        transcript_file.write(transcription)
+
+    print("Created English Transcript")
+
+
+# 3 Translate SRT
+def Translate_srt(target_language):
+    input_srt = "temp/transcript.srt"
+    output_srt = "temp/translated.srt"
+
+    subs = pysrt.open(input_srt, encoding="utf-8")
+
+    for sub in subs:
+        translatedSub = translate_english_to(sub.text, target_language)
+        sub.text = translatedSub
+
+    subs.save(output_srt, encoding="utf-8")
+
+
+# 4 Create Video with Subtitles burned to it (Hard Subtitles)
+def add_subtitle_to_video(subtitle_file):
+    video_input_stream = ffmpeg.input(f'./temp/' + input_video)
+    output_video = f"temp/output_subbed.mp4"
+
+    stream = ffmpeg.output(
+        video_input_stream, output_video, vf=f"subtitles={subtitle_file}"
+    )
+
+    ffmpeg.run(stream, overwrite_output=True)
+
+
+def create_subbed_video(url, language):
+    clear_temp_folder()
+    extract_audio_from_youtube(url)
+    Create_SRT()
+    Translate_srt(language)
+    add_subtitle_to_video("temp/translated.srt")
+
+# url = "https://www.youtube.com/watch?v=QspjKVTMlL8"
+# language = 'yo'
+# create_subbed_video(url, language)
